b65b561069da1324845480f51c298123603ce05a
Serj Tigranyan <serj-tigranyan@mail.ru>
2021-10-26 19:31:30 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите y");
+            double y = double.Parse(Console.ReadLine());
+            if (x > 0 && y < 0) Console.WriteLine("точка лежит в четвёртой четверти");
+            else Console.WriteLine("точка не лежит в четвёртой четверти");
+
+        }
+    }
+}
